bac8b5f990c10a0ff2c7fa7b140d484e026aaf21
dariorea <darioreaok@gmail.com>
2025-11-09 03:43:53 -0300

    update 9.11

--- public/scripts/movie.js
+++ public/scripts/movie.js
@@ -25,14 +25,33 @@ const obtenerPeliculaID = () => {
         const title = document.createElement("h1");
         title.textContent = `${data.title}(${year})`;
 
-        const overview = document.createElement("article")
+        const detailContainer = document.createElement("div")
+        detailContainer.classList.add("container-detail")
+
+        const puntuacion = document.createElement("p")
+        puntuacion.textContent = data.vote_average.toFixed(1)
+        const duracion = document.createElement("p")
+        duracion.textContent = `${data.runtime}min`;
+
+        const overview = document.createElement("em")
         overview.textContent = `${data.overview}`
+
+        const genres = document.createElement("p");
+        const generosText = data.genres.map(g => g.name).join(", ");
+        genres.textContent = `Géneros: ${generosText}`;
+
+        const paises = document.createElement("p")
+        const paisesText = data.production_countries.map(c => c.name).join(", ")
+        paises.textContent = `Paises: ${paisesText}`
+
+        const verEnlace = document.createElement("a")
+        verEnlace.href = data.homepage;
+        verEnlace.textContent = "Ver Ahora"
 
 
         itemImgContainer.appendChild(img)
-        itemInfoContainer.append(title, overview)
-
-
+        detailContainer.append(puntuacion, duracion)
+        itemInfoContainer.append(title, detailContainer, verEnlace ,overview, genres, paises)
     });
 };
 
